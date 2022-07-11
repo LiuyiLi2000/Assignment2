1f0b9f81d2330337300ed591d6910aa1872d0450
LiuyiLi2000 <106932722+LiuyiLi2000@users.noreply.github.com>
2022-07-11 12:59:48 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let pubKey = "pub-c-7b25d17a-dd59-4c86-a215-84bb6d305ee7";
 let subKey = "sub-c-52f810be-3301-4c36-b6d2-0a35ca82ed2a";
 let secretKey = "sec-c-NGIyYzhlYTEtMmM3Ni00ZTc0LTgwNTUtOWIzYjcyZjMzNGNh";
 
-let occupancy = 0; 
+let occupancy; 
 
 let channelName = "presenceTest";
 
